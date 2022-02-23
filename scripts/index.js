8a0c8a944e1fb72ce2d6259d5d8762c45977ceab
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-23 15:09:53 +0100

    added map methods

--- scripts/index.js
+++ scripts/index.js
@@ -3,37 +3,37 @@
 // - in that container grab every single elelemnt
 // - for every element update content
 
-
-
 // card nodes
-const cardStatsNodes = document.querySelectorAll('.card--stats');
-
+const cardStatsNodes = document.querySelectorAll(".card--stats");
 // card array
 const cardStatsArray = [...cardStatsNodes];
 
-// mapping on card
-cardStatsArray.map(card => {
-    // first paragraph of current hours
-    let currentHrs = card.firstElementChild.firstElementChild.innerHTML;
-
-    // last paragraph of previous hours
-    let previousHrs = card.lastElementChild.firstElementChild.innerHTML;
-    console.log(currentHrs + " " + previousHrs)
-})
-
 // links
-const timeTogglers = document.querySelectorAll('.card-user .user--toggle-time div a');
+const timeTogglers = document.querySelectorAll(
+  ".card-user .user--toggle-time div a"
+);
 
 // data
 const url = "./scripts/data.json";
 
 fetch(url)
-    .then((response) => {
-        return response.json(); // return response in json
-    })
-    .then((data) => {
-        let stats = data;
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+  .then((response) => {
+    return response.json(); // return response in json
+  })
+  .then((data) => {
+    let stats = data;
+
+    // loop through stats
+    stats.map(stat => {})
+    // loop through cards
+    cardStatsArray.map((card) => {
+      let currentHrs = card.querySelector(".current-hrs");
+      let previousHrs = card.querySelector(".previous-hrs");
+      let calendar = card.querySelector(".calendar");
+    });
+    // add individial stat to individual card
+    
+  })
+  .catch((err) => {
+    console.log(err);
+  });
