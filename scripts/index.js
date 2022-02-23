f66484dc1e71a5b568de0b0e2a1a1c83ab5df4e7
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-23 12:40:12 +0100

    selected paragraphs and links

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+// TODO
+// - grab every single container
+// - in that container grab every single elelemnt
+// - for every element update content
+
+
+
+// card nodes
+const cardStatsNodes = document.querySelectorAll('.card--stats');
+
+// card array
+const cardStatsArray = [...cardStatsNodes];
+
+// mapping on card
+cardStatsArray.map(card => {
+    // first paragraph of current hours
+    let currentHrs = card.firstElementChild.firstElementChild.innerHTML;
+
+    // last paragraph of previous hours
+    let previousHrs = card.lastElementChild.firstElementChild.innerHTML;
+    console.log(currentHrs + " " + previousHrs)
+})
+
+// links
+const timeTogglers = document.querySelectorAll('.card-user .user--toggle-time div a');
+
 // data
 const url = "./scripts/data.json";
 
@@ -7,7 +33,6 @@ fetch(url)
     })
     .then((data) => {
         let stats = data;
-        console.log(stats[0].timeframes.daily.current) // testing data.json
     })
     .catch((err) => {
         console.log(err);
