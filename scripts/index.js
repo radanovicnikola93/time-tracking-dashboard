af00cae37f30b4954e8c15194d6ab7a3688ed192
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-24 10:03:44 +0100

    added innerHTML

--- scripts/index.js
+++ scripts/index.js
@@ -15,28 +15,30 @@ const url = "./scripts/data.json";
 const fetchData = async() => {
   const response = await fetch(url);
   const data = await response.json();
-  console.log( response)
+  console.log(response)
   console.log(data);
   createContainers(data);
 }
 
-const createContainers = activities => {
-  let cards = activities.map(activity => {
+// map through data
+const createContainers = (activities) => {
+  let cards = activities.map((activity) => {
+    // HTML boilerplate
     return `
         <!-- Daily -->
         <div class="daily">
           <div>
-          <h2>${activity.title}</h2>
-          <a href="#"><img src="./images/icon-ellipsis.svg" alt="" /></a>
-        </div>
-        <div class="card--stats">
-          <div>
-            <p><span class="current-hrs">${activity.timeframes.daily.current}</span>hrs</p>
+            <h2>${activity.title}</h2>
+            <a href="#"><img src="./images/icon-ellipsis.svg" alt="" /></a>
           </div>
-          <div>
-            <p>Last <span class="calendar">Yesterday</span> - <span class="previous-hrs">${activity.timeframes.daily.previous}</span>hrs</p>
+          <div class="card--stats">
+            <div>
+              <p><span class="current-hrs">${activity.timeframes.daily.current}</span>hrs</p>
+            </div>
+            <div>
+              <p>Last <span class="calendar">Yesterday</span> - <span class="previous-hrs">${activity.timeframes.daily.previous}</span>hrs</p>
+            </div>
           </div>
-        </div>
         </div>
 
         <!-- Weekly -->
@@ -46,31 +48,35 @@ const createContainers = activities => {
             <a href="#"><img src="./images/icon-ellipsis.svg" alt="" /></a>
           </div>
           <div class="card--stats">
-          <div>
-            <p><span class="current-hrs">${activity.timeframes.weekly.current}</span>hrs</p>
+            <div>
+              <p><span class="current-hrs">${activity.timeframes.weekly.current}</span>hrs</p>
+            </div>
+            <div>
+              <p>Last <span class="calendar">Last month</span> - <span class="previous-hrs">${activity.timeframes.weekly.previous}</span>hrs</p>
+            </div>
           </div>
-          <div>
-            <p>Last <span class="calendar">Last month</span> - <span class="previous-hrs">${activity.timeframes.weekly.previous}</span>hrs</p>
-          </div>
-        </div>
         </div>
 
         <!-- Montlhy -->
         <div class="monthly">
           <div>
-          <h2>${activity.title}</h2>
-          <a href="#"><img src="./images/icon-ellipsis.svg" alt="" /></a>
-        </div>
-        <div class="card--stats">
-          <div>
-            <p><span class="current-hrs">${activity.timeframes.monthly.current}</span>hrs</p>
+            <h2>${activity.title}</h2>
+            <a href="#"><img src="./images/icon-ellipsis.svg" alt="" /></a>
           </div>
-          <div>
-            <p>Last <span class="calendar">Last month</span> - <span class="previous-hrs">${activity.timeframes.monthly.previous}</span>hrs</p>
+          <div class="card--stats">
+            <div>
+              <p><span class="current-hrs">${activity.timeframes.monthly.current}</span>hrs</p>
+            </div>
+            <div>
+              <p>Last <span class="calendar">Last month</span> - <span class="previous-hrs">${activity.timeframes.monthly.previous}</span>hrs</p>
+            </div>
           </div>
         </div>
-        </div>
-
         `;
-  })
+  });
+  const card = document.querySelector('.card');
+  cards = cards.join('');
+  card.innerHTML = cards;
+
 }
+
